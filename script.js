34935f5ddf5ffcc3349e704c69eb6b4d722ff6a5
Khadidja <khadija.aitsiali@gmail.com>
2025-05-23 13:31:30 +0200

    done toggle dark mode + responsive

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// MENU BURGER
+// menu
 const burger = document.querySelector(".burger");
 const navMenu = document.querySelector(".nav_menu");
 
@@ -17,3 +17,17 @@ function closeMenu() {
     burger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// toggle
+const toggle = document.getElementById('theme_toggle');
+const body = document.body;
+const logo = document.getElementById('logo');
+const sunIcon = document.getElementById('sun_icon');
+const moonIcon = document.getElementById('moon_icon');
+
+toggle.addEventListener('change', () => {
+  const isDark = body.classList.toggle('dark');
+  logo.src = isDark ? 'assets/logo_dark.svg' : 'assets/logo_light.svg';
+  sunIcon.src = isDark ? 'assets/sun_dark.svg' : 'assets/sun_light.svg';
+  moonIcon.src = isDark ? 'assets/moon_light.svg' : 'assets/moon_dark.svg';
+});
